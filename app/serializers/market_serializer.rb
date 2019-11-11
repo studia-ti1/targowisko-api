e5782b995402425e481d2c7f3d6b6cd3c0099b7f
GrzegorzKabzinski <grzegorz.kabzinski@htdevelopers.com>
2019-11-11 15:55:36 +0100

    Fix serializer

--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class MarketSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :facebook_event_id, :location
+  attributes :id, :name, :description, :facebook_event_id, :location, :owner
 
   belongs_to :user
   has_many :products
   has_many :market_ratings
+
+  def owner
+    object.user
+  end
 end
