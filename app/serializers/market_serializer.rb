dc8362d86392da296948319cbd2eda51450664bc
GrzegorzKabzinski <grzegorz.kabzinski@htdevelopers.com>
2019-11-25 19:52:07 +0100

    Return sellers in market serializer

--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class MarketSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :facebook_event_id, :place, :owner, :image, :average_rating, :starts_at, :ends_at
+  attributes :id, :name, :description, :facebook_event_id, :place, :owner, :image, :average_rating, :starts_at, :ends_at, :sellers
 
   belongs_to :user
   has_many :products
@@ -9,5 +9,10 @@ class MarketSerializer < ActiveModel::Serializer
 
   def owner
     object.user
+  end
+
+  def sellers
+    sellers_id = object.products.pluck(:user_id).uniq
+    User.where(id: sellers_id)
   end
 end
