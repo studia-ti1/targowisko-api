86d7c4dc3fec72d362d2b7a775c5245fc6076a3e
Tomasz Kot <kottomasz98@gmail.com>
2019-11-27 00:16:15 +0100

    Intial config before testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,20 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://targowisko.herokuapp.com/'
+  config.action_mailer.default_url_options = { host: host }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: 'targowisko.team@gmail.com',
+    password: 'Password1!!',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
