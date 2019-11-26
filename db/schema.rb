823f87be2fb34cb96004cc8f38df05b849ce11aa
Tomasz Kot <kottomasz98@gmail.com>
2019-11-26 23:36:25 +0100

    Added updating avatar enpoint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_131607) do
+ActiveRecord::Schema.define(version: 2019_11_26_083545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 2019_11_25_131607) do
     t.datetime "updated_at", null: false
     t.integer "products_count"
     t.float "average_rating"
+    t.string "avatar"
   end
 
   add_foreign_key "market_ratings", "markets"
