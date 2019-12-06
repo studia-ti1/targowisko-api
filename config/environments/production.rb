c4034547b5dc446577e6a956777b4dd3ed0ec940
makz21 <maks.regula132@gmail.com>
2019-12-06 01:00:56 +0100

    Another smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,8 +51,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    user_name: 'targowisko.team',
-    password: 'Password1!!',
+    user_name: 'mareczekNetguru123',
+    password: 'trudnehaslo',
     authentication: 'plain',
     enable_starttls_auto: true
   }
